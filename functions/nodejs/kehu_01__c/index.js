8b694d90f06253092bffbe35f988686d5b019a50
1786243250503732.feishu.cn <1786243250503732@feishu.cn>
2023-12-26 11:55:29 +0000

    1 个文件产生了变更，影响了1 个 函数
    更新函数「kehu_01__c」

--- functions/nodejs/kehu_01__c/index.js
+++ functions/nodejs/kehu_01__c/index.js
@@ -11,7 +11,8 @@
  */
 module.exports = async function (params, context, logger) {
   // 日志功能
-  // logger.info(`${new Date()} 函数开始执行`);
+  logger.info(`${new Date()} 函数开始执行`);
+  logger.info(params);
 
   // 在这里补充业务代码
 }
