1bb61cf052b45dc10af7c1f98bc453c88c59506f
OAuth <OAuth@aeservice.com>
2023-12-26 15:34:57 +0800

    system_auto_commit

---
+++ components/test01__c/index.tsx
@@ -0,0 +1,8 @@
+// 自定义组件开发请参考：https://ae.feishu.cn/hc/zh-CN/articles/503630516848
+export interface Props {}
+
+const Test01__c = (props: Props): JSX.Element => {
+  return <span>Hello world!</span>;
+};
+
+export default Test01__c;
